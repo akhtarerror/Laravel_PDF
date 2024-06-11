ae5aade5f0afd11cc008c939d25660a828fbec8b
Akhtar Faizi Putra <akhtarerror@gmail.com>
2024-06-11 09:53:58 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//route resource for products
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
+
+use App\Http\Controllers\PDFController;
+
+Route::get('generate-pdf', [PDFController::class, 'generatePDF'])->name('products.pdf');
